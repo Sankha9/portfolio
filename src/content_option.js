0a46c78314260259cbbbaae821b5c289174089c2
Sankha9 <sankha.softech@gmail.com>
2023-12-28 17:27:22 +0530

    Initial commit

---
+++ src/content_option.js
@@ -0,0 +1,163 @@
+const logotext = "SANKHA NARAYAN SARKAR";
+const meta = {
+    title: "SANKHA NARAYAN SARKAR",
+    description: "I'm SANKHA NARAYAN SARKAR, a Programmer",
+};
+
+const introdata = {
+    title: "Hi, I'm SANKHA NARAYAN SARKAR",
+    animated: {
+        // first: "Technologist",
+        // second: "Visionary",
+        // third: "Ethical Hacker",
+        fourth: "Programmer",
+        fifth: "MERN Developer",
+        sixth: "Analyst",
+        // add more if you'd like but make sure to update /src/pages/home/index.js Line 29
+    },
+    description: "Dedicated technology enthusiast with an insatiable curiosity for unraveling the complexities of our digital world.",
+    your_img_url: "https://i.ibb.co/wRjcnGL/me.jpg",
+};
+
+const dataabout = {
+    title: "Who am I?",
+    aboutme: "Hi, how are you",
+};
+const worktimeline = [{
+        jobtitle: "Example 1",
+        where: "Google",
+        date: "2023",
+    },
+    {
+        jobtitle: "Example 2",
+        where: "Micosoft",
+        date: "2022",
+    },
+    {
+        jobtitle: "Example 3",
+        where: "Meta",
+        date: "2021",
+    },
+];
+
+const skills = [{
+        name: "Node",
+    },
+    
+    {
+        name: "React",
+    },
+    
+    {
+        name: "Java",
+    },
+    
+    {
+        name: "Bash",
+    },
+
+    {
+        name: "Powershell",
+    },
+
+    {
+        name: "Mongodb",
+    },
+
+    {
+        name: "Html",
+    },
+
+    {
+        name: "PostMan",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "sankha96.sarkar@gmail.com",
+    description: "Looking forward to hearing from you!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "SERVICE_ID",
+    YOUR_TEMPLATE_ID: "TEMPLATE_ID",
+    YOUR_USER_ID: "USER_ID (PUBLIC_KEY)",
+};
+
+const socialprofils = {
+    github: "https://github.com/",
+    linkedin: "https://linkedin.com/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
